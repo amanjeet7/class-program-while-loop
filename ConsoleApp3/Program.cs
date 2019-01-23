56c20fbd05dbbd0c3cb401f162b6fd282137a1e0
amanjeet7 <amanjeetbajwa7@gmal.com>
2019-01-23 11:45:40 -0500

    change something

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            int i = 1;
+
+            while (i <= 5)
+            {
+                sum += i;
+                i++;
+            }
+                Console.WriteLine("sum is {0}", sum);
+            
         }
     }
 }
